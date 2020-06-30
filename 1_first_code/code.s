13a2919cb9f5f8047846c9a4e6bb9c5aa863e673
AaronZhang <461177348@qq.com>
2020-06-30 13:30:17 +0800

    Add files via upload

---
+++ 1_first_code/code.s
@@ -0,0 +1,24 @@
+	.file	"code.c"
+	.text
+	.p2align 4,,15
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	12(%ebp), %eax
+	addl	8(%ebp), %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.ident	"GCC: (Uos 8.3.0.3-3+rebuild) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ 2_cmovle/code.s
@@ -0,0 +1,35 @@
+	.file	"code.c"
+	.text
+	.globl	absdiff
+	.type	absdiff, @function
+absdiff:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	-20(%rbp), %eax
+	cmpl	-24(%rbp), %eax
+	jle	.L2
+	movl	-20(%rbp), %eax
+	subl	-24(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	jmp	.L3
+.L2:
+	movl	-24(%rbp), %eax
+	subl	-20(%rbp), %eax
+	movl	%eax, -4(%rbp)
+.L3:
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	absdiff, .-absdiff
+	.ident	"GCC: (Uos 8.3.0.3-3+rebuild) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
